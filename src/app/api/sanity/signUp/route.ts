5ac37334de31bf1b66d18f059653e1731ccf010a
hrfahimm <fahimtaj10@gmail.com>
2024-01-25 00:14:53 +0600

    rrr

--- src/app/api/sanity/signUp/route.ts
+++ src/app/api/sanity/signUp/route.ts
@@ -1,6 +1,7 @@
-import { signUpHandler } from 'next-auth-sanity';
-
 import sanityClient from '@/libs/sanity';
+import { signUpHandler } from 'next-auth-sanity';
+import { SanityClient } from 'sanity';
 
-export const POST = signUpHandler(sanityClient);
+
+export const POST = signUpHandler(sanityClient as SanityClient);
 
--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -3,6 +3,7 @@ import { SanityAdapter, SanityCredentials } from 'next-auth-sanity';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import sanityClient from './sanity';
+import { SanityClient } from 'sanity';
 
 
 
@@ -18,12 +19,12 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
-        SanityCredentials(sanityClient),
+        SanityCredentials(sanityClient as SanityClient),
     ],
     session: {
         strategy: 'jwt',
     },
-    adapter: SanityAdapter(sanityClient),
+    adapter: SanityAdapter(sanityClient as SanityClient),
     debug: process.env.NODE_ENV === 'development',
     secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
