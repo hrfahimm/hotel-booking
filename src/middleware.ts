44e75c1574c27835e6a2440d7833ba704ed2296c
hrfahimm <fahimtaj10@gmail.com>
2024-01-24 23:38:41 +0600

    last

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 export { default } from 'next-auth/middleware'
 
 
-// export const config = {
-//     matcher: ['/users/:path*']
-
-// }
-
 export const config = {
-    matcher: ['/users/:path*', '/api/:path*']
+    matcher: ['/users/:path*']
 
 }
+
+// export const config = {
+//     matcher: ['/users/:path*', '/api/:path*']
+
+// }
