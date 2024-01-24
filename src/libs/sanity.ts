0d50a0a660e67ee526c516ec74e32df961e30102
hrfahimm <fahimtaj10@gmail.com>
2024-01-25 00:31:40 +0600

    sss

--- src/libs/sanity.ts
+++ src/libs/sanity.ts
@@ -1,12 +1,13 @@
 import { createClient, type ClientConfig } from '@sanity/client'
 
-const client = createClient({
+const config: ClientConfig = {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     useCdn: process.env.NODE_ENV === 'production',
     token: process.env.SANITY_STUDIO_TOKEN,
     apiVersion: '2023-10-16',
-});
+}
+const client = createClient(config)
 
 export default client;
 
